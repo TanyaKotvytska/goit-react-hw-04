af4e81123a960b32e32657569abeafd31e86a619
Tanya Kotvytska <tanyakotvitskaya20@gmail.com>
2024-08-28 09:45:01 +0300

    add css

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -2,6 +2,6 @@ import css from './LoadMoreBtn.module.css'
 
 export default function LoadMoreBtn({ onLoadMore }) {
     return (
-        <button className={css.button} onClick={onLoadMore}>Load more</button>
+        <button className={css.btnloadmore} onClick={onLoadMore}>Load more</button>
     );
 }
