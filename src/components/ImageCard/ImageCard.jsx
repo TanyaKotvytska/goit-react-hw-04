c2bc681d510deb3921ccd48bbc2304b474c888a5
Tanya Kotvytska <tanyakotvitskaya20@gmail.com>
2024-08-30 13:31:55 +0300

    update

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,8 +2,8 @@ import css from './ImageCard.module.css';
 
 export default function ImageCard({ image, onClick }) {
     return (
-        <div className={css.card} onClick={() => onClick(image)}>
-            <img src={image.urls.small} alt={image.alt_description} className={css.img} />
+        <div className={css.card}>
+            <img src={image.urls.small} alt={image.alt_description} className={css.img} onClick={() => onClick(image)} />
     </div>
     )
 }
